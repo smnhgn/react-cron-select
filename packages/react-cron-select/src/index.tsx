dabafd30bf6486207e5d2abf58b08f470a60d4b9
Simon Haugen <smn.hgn@gmail.com>
2022-04-23 19:12:22 +0200

    added cron-parser

--- packages/react-cron-select/src/index.tsx
+++ packages/react-cron-select/src/index.tsx
@@ -1,9 +1,8 @@
-import React, { ElementType } from "react";
+import { CronFields, parseExpression, fieldsToExpression } from "cron-parser";
+import React, { ChangeEvent, ElementType, useState } from "react";
+import { CRON_PERIODS, CRON_FIELDS, CronPeriod, INITIAL_CRON_FIELDS } from "./constants";
 
-export interface CronSelectProps<
-  SelectProps = Record<string, any>,
-  OptionProps = Record<string, any>
-> {
+export interface CronSelectProps<SelectProps = any, OptionProps = Record<string, any>> {
   className?: string;
   Select?: ElementType;
   Option?: ElementType;
@@ -18,17 +17,74 @@ export const CronSelect = <SelectProps, OptionProps>({
   selectProps,
   optionProps,
 }: CronSelectProps<SelectProps, OptionProps>) => {
-  const periods = ["year", "month", "week", "day", "hour", "minute"];
+  const isNativeSelect = Select === "select";
+  const interval = parseExpression("* * * * * *");
+  const [period, setPeriod] = useState<CronPeriod>();
+  const [fields, setFields] = useState<CronFields>(interval.fields);
+  const [expression, setExpression] = useState<string>();
+  const [error, setError] = useState<Error | null>();
+
+  const handlePeriodChange = (e: ChangeEvent<any>) => {
+    setPeriod(e.target.value);
+  };
+
+  const handleFieldChange = (e: ChangeEvent<any>) => {
+    setError(null);
+
+    const fieldValue = (
+      isNativeSelect
+        ? Array.from((e.target as HTMLSelectElement).selectedOptions).map((option) => option.value)
+        : e.target.value
+    ).map((v: string) => parseInt(v));
+
+    const newFields = { ...fields, [e.target.name]: fieldValue };
+
+    setFields(newFields);
+
+    try {
+      setExpression(fieldsToExpression(newFields).stringify());
+    } catch (error) {
+      setError(error as Error);
+    }
+  };
 
   return (
     <div className={className}>
-      <Select {...selectProps} defaultValue={periods[0]}>
-        {periods.map((period) => (
-          <Option {...optionProps} value={period}>
-            {period}
+      <Select
+        {...selectProps}
+        key="period"
+        name="period"
+        onChange={handlePeriodChange}
+        defaultValue=""
+      >
+        <Option {...optionProps} key="empty" value="" disabled></Option>
+        {Object.keys(CRON_PERIODS).map((periodOptionKey) => (
+          <Option {...optionProps} key={periodOptionKey} value={periodOptionKey}>
+            {periodOptionKey}
           </Option>
         ))}
       </Select>
+
+      {period &&
+        CRON_PERIODS[period].map((fieldOptionKey) => (
+          <Select
+            {...selectProps}
+            key={`${period}_${fieldOptionKey}`}
+            name={fieldOptionKey}
+            onChange={handleFieldChange}
+            defaultValue={[]}
+            multiple
+          >
+            <Option {...optionProps} key="empty" value="" disabled></Option>
+            {Object.keys(CRON_FIELDS[fieldOptionKey]).map((fieldOptionValue) => (
+              <Option {...optionProps} key={fieldOptionValue} value={fieldOptionValue}>
+                {fieldOptionValue}
+              </Option>
+            ))}
+          </Select>
+        ))}
+
+      <div>{error ? error?.message : expression}</div>
     </div>
   );
 };
