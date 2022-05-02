d7922e6f2f8a89ef06e216f6ab786eaa258c1910
Simon Haugen <smn.hgn@gmail.com>
2022-05-02 19:55:03 +0200

    (wip) use popover with tabs instead of multiple selects

--- packages/react-cron-select-demo/src/App.tsx
+++ packages/react-cron-select-demo/src/App.tsx
@@ -1,19 +1,10 @@
 import React, { ComponentProps } from "react";
 import { CronSelect } from "react-cron-select";
-import Select, { SelectProps } from "@mui/material/Select";
-import MenuItem, { MenuItemProps } from "@mui/material/MenuItem";
-import Divider from "@mui/material/Divider";
 
 function App() {
   return (
     <div>
       <CronSelect />
-      <Divider sx={{ my: 2 }} />
-      <CronSelect<SelectProps, MenuItemProps>
-        Select={Select}
-        Option={MenuItem}
-        selectProps={{ size: "small" }}
-      />
     </div>
   );
 }
--- packages/react-cron-select/src/index.tsx
+++ packages/react-cron-select/src/index.tsx
@@ -1,90 +1,100 @@
 import { CronFields, parseExpression, fieldsToExpression } from "cron-parser";
-import React, { ChangeEvent, ElementType, useState } from "react";
-import { CRON_PERIODS, CRON_FIELDS, CronPeriod, INITIAL_CRON_FIELDS } from "./constants";
+import React, { useState, MouseEvent, SyntheticEvent, useEffect } from "react";
+import TextField from "@mui/material/TextField";
+import Popover from "@mui/material/Popover";
+import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import ToggleButton from "@mui/material/ToggleButton";
 
-export interface CronSelectProps<SelectProps = any, OptionProps = Record<string, any>> {
-  className?: string;
-  Select?: ElementType;
-  Option?: ElementType;
-  selectProps?: SelectProps;
-  optionProps?: OptionProps;
-}
+const DEFAULT_INTERVAL = parseExpression("* * * * * *");
 
-export const CronSelect = <SelectProps, OptionProps>({
-  className = "react-cron-select",
-  Select = "select",
-  Option = "option",
-  selectProps,
-  optionProps,
-}: CronSelectProps<SelectProps, OptionProps>) => {
-  const isNativeSelect = Select === "select";
-  const interval = parseExpression("* * * * * *");
-  const [period, setPeriod] = useState<CronPeriod>();
-  const [fields, setFields] = useState<CronFields>(interval.fields);
-  const [expression, setExpression] = useState<string>();
+type CronField = keyof CronFields;
+
+export interface CronSelectProps {}
+
+export const CronSelect = ({}: CronSelectProps) => {
+  const [period, setPeriod] = useState<CronField>("month");
+  const [fields, setFields] = useState<CronFields>(DEFAULT_INTERVAL.fields);
+  const [expression, setExpression] = useState<string>(DEFAULT_INTERVAL.stringify(true));
   const [error, setError] = useState<Error | null>();
+  const [anchorEl, setAnchorEl] = useState<HTMLInputElement | null>(null);
+  const open = Boolean(anchorEl);
+  const id = open ? "react-cron-select-popover" : undefined;
 
-  const handlePeriodChange = (e: ChangeEvent<any>) => {
-    setPeriod(e.target.value);
+  const handleOpen = (e: MouseEvent<HTMLInputElement>) => {
+    setAnchorEl(e.currentTarget);
   };
 
-  const handleFieldChange = (e: ChangeEvent<any>) => {
-    setError(null);
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    const fieldValue = (
-      isNativeSelect
-        ? Array.from((e.target as HTMLSelectElement).selectedOptions).map((option) => option.value)
-        : e.target.value
-    ).map((v: string) => parseInt(v));
+  const handleChangePeriod = (e: SyntheticEvent<any>, newValue: CronField) => {
+    setPeriod(newValue);
+  };
 
-    const newFields = { ...fields, [e.target.name]: fieldValue };
+  const handleChangeMonth = (e: SyntheticEvent<any>, newValue: CronFields["month"]) => {
+    setFields({ ...fields, month: newValue });
+  };
 
-    setFields(newFields);
+  const handleChangeDayOfMonth = (e: SyntheticEvent<any>, newValue: CronFields["dayOfMonth"]) => {
+    setFields({ ...fields, dayOfMonth: newValue });
+  };
 
+  useEffect(() => {
     try {
-      setExpression(fieldsToExpression(newFields).stringify());
-    } catch (error) {
-      setError(error as Error);
+      setExpression(fieldsToExpression(fields).stringify(true));
+    } catch (err) {
+      setError(err as Error);
     }
-  };
+  }, [fields]);
 
   return (
-    <div className={className}>
-      <Select
-        {...selectProps}
-        key="period"
-        name="period"
-        onChange={handlePeriodChange}
-        defaultValue=""
+    <Box>
+      <TextField aria-describedby={id} value={expression} onClick={handleOpen} />
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
       >
-        <Option {...optionProps} key="empty" value="" disabled></Option>
-        {Object.keys(CRON_PERIODS).map((periodOptionKey) => (
-          <Option {...optionProps} key={periodOptionKey} value={periodOptionKey}>
-            {periodOptionKey}
-          </Option>
-        ))}
-      </Select>
+        <Box>
+          <Tabs value={period} onChange={handleChangePeriod}>
+            <Tab value="month" label="month" />
+            <Tab value="dayOfMonth" label="month" />
+            <Tab value="dayOfWeek" label="dayOfWeek" />
+            <Tab value="hour" label="hour" />
+            <Tab value="minute" label="minute" />
+            <Tab value="second" label="second" />
+          </Tabs>
 
-      {period &&
-        CRON_PERIODS[period].map((fieldOptionKey) => (
-          <Select
-            {...selectProps}
-            key={`${period}_${fieldOptionKey}`}
-            name={fieldOptionKey}
-            onChange={handleFieldChange}
-            defaultValue={[]}
-            multiple
-          >
-            <Option {...optionProps} key="empty" value="" disabled></Option>
-            {Object.keys(CRON_FIELDS[fieldOptionKey]).map((fieldOptionValue) => (
-              <Option {...optionProps} key={fieldOptionValue} value={fieldOptionValue}>
-                {fieldOptionValue}
-              </Option>
-            ))}
-          </Select>
-        ))}
+          {period === "month" && (
+            <ToggleButtonGroup value={fields.month} onChange={handleChangeMonth}>
+              {DEFAULT_INTERVAL.fields.month.map((monthValue) => (
+                <ToggleButton key={monthValue} value={monthValue}>
+                  {monthValue}
+                </ToggleButton>
+              ))}
+            </ToggleButtonGroup>
+          )}
 
-      <div>{error ? error?.message : expression}</div>
-    </div>
+          {period === "dayOfMonth" && (
+            <ToggleButtonGroup value={fields.dayOfMonth} onChange={handleChangeDayOfMonth}>
+              {DEFAULT_INTERVAL.fields.dayOfMonth.map((dayOfMonthValue) => (
+                <ToggleButton key={dayOfMonthValue} value={dayOfMonthValue}>
+                  {dayOfMonthValue}
+                </ToggleButton>
+              ))}
+            </ToggleButtonGroup>
+          )}
+        </Box>
+      </Popover>
+    </Box>
   );
 };
